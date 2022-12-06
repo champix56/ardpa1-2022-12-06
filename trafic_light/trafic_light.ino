1a29e9e6e5397395b9e81045e8b753f70346f2fe
champix56 <desorbaix@free.fr>
2022-12-06 16:54:25 +0100

    digitalWrite(PIN,STATE)

--- trafic_light/trafic_light.ino
+++ trafic_light/trafic_light.ino
@@ -8,5 +8,16 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  digitalWrite(4, HIGH);
+  delay(1000);
+  digitalWrite(5, HIGH);
+  delay(1000);
+  digitalWrite(6, HIGH);
+  delay(1000);
+  digitalWrite(4, LOW);
+  delay(1000);
+  digitalWrite(5, LOW);
+  delay(1000);
+  digitalWrite(6, LOW);
+  delay(500);
 }
