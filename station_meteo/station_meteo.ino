fa0e90824261c3909849c3ee898cb3d713a41a23
champix56 <desorbaix@free.fr>
2022-12-07 17:06:28 +0100

    base DHT11 Signed-off-by: champix56 <desorbaix@free.fr>

---
+++ station_meteo/station_meteo.ino
@@ -0,0 +1,28 @@
+#include "DHT.h"
+
+#define DHTPIN 2  
+#define DHTTYPE DHT11  
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  dht.begin();
+  Serial.println(F("METEO STATION"));
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+ float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  float f = dht.readTemperature(true);
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+
+}
