46c377fdda047afb426dbaa7d2aa50cddbc866fa
champix56 <desorbaix@free.fr>
2022-12-08 15:10:18 +0100

    protection inclusions multiple ou cyclique

--- station_meteo/structs.h
+++ station_meteo/structs.h
@@ -1,12 +1,15 @@
-#include <TimeLib.h>
+#ifndef _STRUCTS_H_
+  #define _STRUCTS_H_
+  #include <TimeLib.h>
 
-struct S_Humidity{
-  float temp;
-  float rhumidity;
-};
-typedef struct S_Datas{
-    struct S_Humidity humidity;
-    tmElements_t time;
-};
-//typedef struct S_Data tmData;
-//si usage de tmData datas pour la decla
+  struct S_Humidity{
+    float temp;
+    float rhumidity;
+  };
+  typedef struct S_Datas{
+      struct S_Humidity humidity;
+      tmElements_t time;
+  };
+  //typedef struct S_Data tmData;
+  //si usage de tmData datas pour la decla
+#endif
