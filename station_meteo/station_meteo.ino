76f83785ee41d54d4fa7983c4c81d5a4dc715b62
champix56 <desorbaix@free.fr>
2022-12-08 14:55:31 +0100

    manipulation et formatage chaine

--- station_meteo/station_meteo.ino
+++ station_meteo/station_meteo.ino
@@ -18,17 +18,27 @@ void loop() {
   char str[33] = "";
   formatDatasStr(datas, str);
   Serial.print(str);
+  delay(930);
 }
 uint8_t formatDatasStr(S_Datas datas, char buffer[]) {
+  char buffRh[6] = "";
+  char buffTemp[6] = "";
   //yyyy-mm-ddThh:mm:ss;rr.rr;tt.tt\n
   //ecriture du temps au format YYYY-MM-DDTHH:mm:ss;
-  uint8_t length = sprintf(buffer, "%4d-%02d-%02dT%02d:%02d:%02d;",
-                           (datas.time.Year + 1970),
-                           datas.time.Month,
-                           datas.time.Day,
-                           datas.time.Hour,
-                           datas.time.Minute,
-                           datas.time.Second);
-  
-  return length;
+  sprintf(buffer, "%4d-%02d-%02dT%02d:%02d:%02d;%5s;%5s\n",
+          (datas.time.Year + 1970),
+          datas.time.Month,
+          datas.time.Day,
+          datas.time.Hour,
+          datas.time.Minute,
+          datas.time.Second,
+          dtostrf(datas.humidity.rhumidity, 2, 2, buffRh),
+          dtostrf(datas.humidity.temp, 2, 2, buffTemp));
+
+  //format decimal to buffer string
+  //dtostrf(datas.humidity.rhumidity, 2, 2, buffFloat);
+  //concat buffFloat vers buffer
+  //strcat(buffer,buffFloat);
+  //taille (en nb de caracteres) d'une chaine
+  return strlen(buffer);
 }
